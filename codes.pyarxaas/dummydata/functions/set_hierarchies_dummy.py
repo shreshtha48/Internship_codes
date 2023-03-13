25f723fb7b635f22b199496ae4f58465a269a632
Shreshtha <modishreshtha48@gmail.com>
2023-03-13 16:01:01 +0530

    Update set_hierarchies_dummy.py

--- codes.pyarxaas/dummydata/functions/set_hierarchies_dummy.py
+++ codes.pyarxaas/dummydata/functions/set_hierarchies_dummy.py
@@ -1,47 +1,37 @@
-#this function sets the hierarchy for the datasetto get it ready for the anonymization
-#load the data and the neccesary libraries
-#we will start by importing the neccesarry libraries
-import pandas as pd
-#we will start by importing the neccesarry libraries
-import numpy as np
-import pandas as pd
-#pyarxaas will allow us to import some functions neccesary for anonymization
+
 import pyarxaas
 from pyarxaas import ARXaaS
 from pyarxaas.hierarchy import IntervalHierarchyBuilder, OrderHierarchyBuilder,RedactionHierarchyBuilder
 from pyarxaas import AttributeType
 from pyarxaas.privacy_models import KAnonymity
 from pyarxaas import Dataset
+import pandas as pd
+import pandas as pd
+
 def set_hierarchy(data=pd.DataFrame):
-    dataset=Dataset.from_pandas(data)
-    #starting up the arx instance:
+    name_hierarchy = pd.read_csv("/home/shreshtha/Hiearchies/namehier.csv")
+    age_hierarchy = pd.read_csv("/home/shreshtha/Hiearchies/agehier.csv")
+    mail_hierarchy = pd.read_csv("/home/shreshtha/Hiearchies/emailhier.csv")
+    gender_hierarchy = pd.read_csv("/home/shreshtha/Hiearchies/gender.csv")
+    zip_hierarchy = pd.read_csv("/home/shreshtha/Hiearchies/zipcode.csv")
+    amount_spent_hierarchy = pd.read_csv("/home/shreshtha/Hiearchies/amounthier.csv")
+
+    dataset = Dataset.from_pandas(data)
     arxaas = ARXaaS("http://localhost:8080/")
-    #setting the attribute type from arxaas
-    dataset.set_attribute_type(AttributeType.QUASIIDENTIFYING, 'Age','Email','zipcode','Amount_spent','Gender','Name')
-    #setting the hierarchies
-    #the following iterates through the columns of the data and sets the hierarchies based on the same
-    for columns in data:
-        if columns=='Name':
-            name_hierarchy=pd.read_csv("/home/shreshtha/Hiearchies/namehier.csv")
-            dataset.set_hierarchy('Name',name_hierarchy)
-        elif columns=='Age':
-            age_hierarchy=pd.read_csv("/home/shreshtha/Hiearchies/agehier.csv")
-            dataset.set_hierarchy('Age',age_hierarchy)
-        elif columns=='Email':
-            mail_hierarchy=pd.read_csv("/home/shreshtha/Hiearchies/emailhier.csv")
-            dataset.set_hierarchy('Email',mail_hierarchy)
-        elif columns=='Gender':
-            gender_hierarchy=pd.read_csv("/home/shreshtha/Hiearchies/gender.csv")
-            dataset.set_hierarchy('Gender',gender_hierarchy)
-        elif columns=='Zipcode':
-            zip_hierarchy=pd.read_csv("/home/shreshtha/Hiearchies/zipcode.csv")
-            dataset.set_hierarchy('zipcode',zip_hierarchy)
+    dataset.set_attribute_type(AttributeType.QUASIIDENTIFYING, 'Age', 'Email', 'zipcode', 'Amount_spent', 'Gender', 'Name')
+    for column in ['Age', 'Email', 'zipcode', 'Amount_spent', 'Gender', 'Name']:
+        if column not in data.columns:
+            raise ValueError(f"{column} column not found in data")
+        if column == 'Name':
+            dataset.set_hierarchy('Name', name_hierarchy)
+        elif column == 'Age':
+            dataset.set_hierarchy('Age', age_hierarchy)
+        elif column == 'Email':
+            dataset.set_hierarchy('Email', mail_hierarchy)
+        elif column == 'Gender':
+            dataset.set_hierarchy('Gender', gender_hierarchy)
+        elif column == 'Zipcode':
+            dataset.set_hierarchy('zipcode', zip_hierarchy)
         else:
-            amount_spent_hierarchy=pd.read_csv("/home/shreshtha/Hiearchies/amounthier.csv")
-            dataset.set_hierarchy('Amount_spent',amount_spent_hierarchy)
-    return
-# a demo of the function
-data=pd.read_csv("/home/shreshtha/Downloads/customer_data.csv")
-set_hierarchy(data)
-
-
+            dataset.set_hierarchy('Amount_spent', amount_spent_hierarchy)
+    return dataset
