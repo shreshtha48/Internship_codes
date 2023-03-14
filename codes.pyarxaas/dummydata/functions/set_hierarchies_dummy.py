01e1dcfd14d980471cd304f9b4ffc180fa66578b
Shreshtha <modishreshtha48@gmail.com>
2023-03-14 12:14:13 +0530

    Update set_hierarchies_dummy.py

--- codes.pyarxaas/dummydata/functions/set_hierarchies_dummy.py
+++ codes.pyarxaas/dummydata/functions/set_hierarchies_dummy.py
@@ -1,3 +1,4 @@
+#imported all the neccessary libraries
 import pyarxaas
 from pyarxaas import ARXaaS, Dataset
 from pyarxaas import AttributeType
@@ -5,15 +6,9 @@ from pyarxaas.hierarchy import IntervalHierarchyBuilder, OrderHierarchyBuilder, 
 from pyarxaas.privacy_models import KAnonymity
 import pandas as pd
 import pickle
-
-import pyarxaas
-from pyarxaas import ARXaaS, Dataset
-from pyarxaas.hierarchy import IntervalHierarchyBuilder, OrderHierarchyBuilder, RedactionHierarchyBuilder
-from pyarxaas.privacy_models import KAnonymity
-import pandas as pd
-import pickle
-
+#making a function that takes in the dataframe as input and also has the choice of generating the risk profile
 def set_hierarchy(data=pd.DataFrame, risk_profile=False):
+    #files which contain the csv files to the hierarchy, they contain the local path for now but will add the global path once it is figured out
     hierarchy_files = {
         "Name": "/home/shreshtha/Hiearchies/namehier.csv",
         "Age": "/home/shreshtha/Hiearchies/agehier.csv",
@@ -22,6 +17,8 @@ def set_hierarchy(data=pd.DataFrame, risk_profile=False):
         "zipcode": "/home/shreshtha/Hiearchies/zipcode.csv",
         "Amount_spent": "/home/shreshtha/Hiearchies/amounthier.csv"
     }
+    #instead of reading through the csv files again and again, this code turns them into pickle objects hence making sure that the code os faster
+    #also added exception handling to check for missing or unavailable files
     hierarchy_data = {}
     for column, file in hierarchy_files.items():
         try:
@@ -31,18 +28,18 @@ def set_hierarchy(data=pd.DataFrame, risk_profile=False):
             hierarchy_data[column] = pd.read_csv(file)
             with open(f"{column.lower()}_hier.pkl", "wb") as f:
                 pickle.dump(hierarchy_data[column], f)
-
+#creating a dataset object from the dataframe
     dataset = Dataset.from_pandas(data)
+    #setting up the local arx instance
     arxaas = ARXaaS("http://localhost:8080/")
+    #setting up the attribute types
     dataset.set_attribute_type(AttributeType.QUASIIDENTIFYING, "Age", "Email", "zipcode", "Amount_spent", "Gender", "Name")
-    if risk_profile:
-        risk_profile = arxaas.risk_profile(dataset)
-        print(risk_profile.re_identification_risk)
-        print(risk_profile.attacker_success_rate)
-        print(risk_profile.distribution_of_risk)
-        return risk_profile
+    #also prints the risk profile if the risk profile is true
+    #raising an error to check for the columns in the data, this can be done because the columns  here are defined already
     for column in ["Name", "Age", "Email", "Gender", "zipcode", "Amount_spent"]:
         if column not in data.columns:
             raise ValueError(f"{column} column not found in data")
+            #setting the hierarchy for the dataset
         dataset.set_hierarchy(column, hierarchy_data[column])
+        
     return dataset
